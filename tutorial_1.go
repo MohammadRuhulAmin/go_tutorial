770af8704cb1bc0034915496d8d8d291827dc443
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-05-17 12:40:47 +0600

    tutorial_1 updated

--- tutorial_1.go
+++ tutorial_1.go
@@ -4,10 +4,13 @@ import "fmt"  // module
 
 func main(){
 
+
 	fmt.Println("Hi , world")    // 'h' is just for the charecter 
 	fmt.Println("this is md ruhul amin")
 	fmt.Println("American International University Bangladesh")
-
+	fmt.Println("ruhulamin.cs.dev@gmail.com")
+	fmt.Println("01322-352864")
+	
 
 }
 
