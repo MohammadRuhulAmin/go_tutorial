26de6d9b0f4169586609d05c64f4c41a49aa8831
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-05-29 12:37:03 +0600

    tutorial_23 all type of data of one array using Interface

---
+++ tutorial_23.go
@@ -0,0 +1,18 @@
+package main 
+
+import (
+	"fmt"
+)
+
+func main(){
+
+	allTypeDataInArray := []interface{}{"RuhulAmin","CSE",148,3.23,"American International University-Bangladesh","01322-352864"}
+
+	for key,val := range allTypeDataInArray{
+		fmt.Println(key," -> ",val)
+	}
+
+
+
+
+}
