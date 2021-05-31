dab9ba25b5d11c275aaf0fdc91dee034b21b9e0c
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-05-31 16:17:24 +0600

    tutorial_32  Using a loop with a channel

---
+++ tutorial_32.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func SendCalculation(inf chan int, val int) {
+	inf <- val * 2
+	inf <- val * 4
+	inf <- val * 8
+
+	close(inf) // closing a channel is must !!!
+
+}
+
+func main() {
+
+	channel := make(chan int)
+	go SendCalculation(channel, 2)
+
+	for val := range channel {
+		fmt.Println(val)
+	}
+
+}
