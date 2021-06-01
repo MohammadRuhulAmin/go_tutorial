d9a05ffd3372f43bc3bca8624fdef1788360b2d4
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-06-01 14:40:59 +0600

    tutorial_37 goroutine waitGroup annonimusFunction

--- tutorial_37.go
+++ tutorial_37.go
@@ -4,16 +4,32 @@ import (
 	"fmt"
 	"sync"
 )
+var wg sync.WaitGroup 
 
 func main(){
-	var wg sync.WaitGroup
+	wg.Add(3)
 	arr  := []string {"Ruhul","Amin","Sakib","Sajid"}
-	wg.Add(1)
+	brr :=[]int{1,2,3,4,5}
+	go func(){
+		for key,val := range brr{
+			fmt.Printf("%d => %d =>",key+1,val)
+		}
+		wg.Done()
+		fmt.Println()
+	}()
 	go func(){
 		for key,val :=range arr{
 			fmt.Println(key," ",val)
 		}
 		wg.Done()
+		
+	}()
+	go func(){
+		for key,val :=range arr{
+			fmt.Println(key," ",val)
+		}
+		wg.Done()
+		
 	}()
 	
 	wg.Wait()
