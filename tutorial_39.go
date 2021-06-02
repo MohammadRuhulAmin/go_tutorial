0f3b9d835f725c4f5dec27aed841df3431c1efa5
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-06-02 11:41:48 +0600

    tutorial_39 mutex  introduction

---
+++ tutorial_39.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func counting(value *int, wg *sync.WaitGroup , mutex *sync.Mutex) {
+	mutex.Lock()
+	*value++
+	mutex.Unlock()
+	wg.Done()
+}
+
+
+
+func main() {
+	var wg sync.WaitGroup 
+	var m sync.Mutex 
+	var val int = 0 
+	for x := 1; x <= 1000; x++ {
+		wg.Add(1)
+		go counting(&val, &wg,&m)
+	}
+	
+	wg.Wait()
+	fmt.Println(val) 
+	fmt.Println("More About Mutex:","https://golangdocs.com/mutex-in-golang")
+}
+
+
